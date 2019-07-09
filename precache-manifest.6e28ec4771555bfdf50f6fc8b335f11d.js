728b4c684858f334bdd4286eb937b6cfdb01a0f9
andypieters <andy@pay.nl>
2019-07-09 09:40:30 +0200

    Updates

---
+++ precache-manifest.6e28ec4771555bfdf50f6fc8b335f11d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ce069cce6d652465022571d7b2e48d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d940b506496b94822288",
+    "url": "/static/css/main.8ba98fe2.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "d940b506496b94822288",
+    "url": "/static/js/main.bdf25b5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
