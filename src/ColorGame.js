249e2b849b7075e69d10172af1d9c7bd117a8dbc
andypieters <andy@pay.nl>
2019-07-10 22:45:36 +0200

    Dark theme

--- src/ColorGame.js
+++ src/ColorGame.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './ColorGame.css';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 class Square extends React.Component {
 
